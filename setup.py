84b0f2821e5dd115d1e8e16db1fde5cefe03eddf
jakeogh <github.com@v6y.net>
2022-10-22 13:19:52 -0700

    autocommit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+
+setup(
+    name='test-single-exception-class-top-module',
+    packages=[],
+    py_modules=['test_single_exception_class_top_module'],
+    install_requires=[],
+    scripts=['test_single_exception_class_top_module.py'],
+    )
